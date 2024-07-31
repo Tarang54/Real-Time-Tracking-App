a2f60f3a2cfe5de8588f4aa3c539e4f907361f3b
Tarang <tarangsultania54@gamil.com>
2024-08-01 00:19:40 +0530

    Added logic

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require('express')
 const app = express()
 const http = require("http")
 const socketio = require("socket.io")
+const path = require("path");
 
 const server = http.createServer(app);
 
 const io = socketio(server)
 
 app.set("view engine" , "ejs");
-app.set(express.static(path.join(__dirname , "public")));
+app.use(express.static(path.join(__dirname , "public")));
+
+io.on("connection", function(socket){
+    
+    socket.on("send-location", function(data){
+        io.emit("receive-location", { id: socket.id, ...data });
+    })
+    console.log("connected");
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id)
+    });
+});
 
 app.get("/", (req, res) => {
-    res.send("Hey")
-})
+    res.render("index")
+});
 
 
-app.listen(3000)
+app.listen(3000);
